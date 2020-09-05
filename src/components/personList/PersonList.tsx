7a8e4eaa68c219e2aafb0a7a57ffc6bb25b3faed
Julio Mixco <julio.mixg@gmail.com>
2020-09-05 12:33:13 -0600

    create person list items

--- src/components/personList/PersonList.tsx
+++ src/components/personList/PersonList.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import { PersonListContent } from './PersonList.style';
+import { Person } from '../../interfaces/person.interface';
+import { PersonItem } from '../personItem/PersonItem';
 
-export const PersonList: React.SFC<{}> = () => (
-  <PersonListContent></PersonListContent>
+interface PersonListProps {
+  people: Person[];
+  onPersonSelected?: (person: Person) => void;
+}
+
+export const PersonList: React.SFC<PersonListProps> = ({
+  people,
+  onPersonSelected,
+}) => (
+  <PersonListContent>
+    {people.map((p, index) => (
+      <PersonItem
+        key={index}
+        person={p}
+        onClick={() => onPersonSelected?.(p)}></PersonItem>
+    ))}
+  </PersonListContent>
 );
+
+PersonList.defaultProps = {
+  onPersonSelected: (person) => {},
+};
--- src/pages/StarWars.tsx
+++ src/pages/StarWars.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Layout } from '../components/layout/Layout';
 import { PersonList } from '../components/personList/PersonList';
-
+import { Person } from '../interfaces/person.interface';
+const person: Person = {
+  name: 'juan',
+  species: [{ name: 'grey' }, { name: 'yellow' }],
+  homeworld: {
+    name: 'mars',
+  },
+} as Person;
 export const StarWars = () => (
   <Layout>
-    <PersonList></PersonList>
+    <PersonList people={[person, person]}></PersonList>
   </Layout>
 );
