0268b296a9d93d7068413f898b79e2e529ff44c8
Julio Mixco <julio.mixg@gmail.com>
2020-09-06 11:58:53 -0600

    Updates

---
+++ precache-manifest.d8b37d1a4d83ec792acbfedb4ee2cb0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b69027b0f63b498ea27c7b85a4b026",
+    "url": "/Ravn-Challenge-V2-Julio-Mixco/index.html"
+  },
+  {
+    "revision": "829af645e95cf157225d",
+    "url": "/Ravn-Challenge-V2-Julio-Mixco/static/css/main.b1f5e050.chunk.css"
+  },
+  {
+    "revision": "545bb49ff13e88af240c",
+    "url": "/Ravn-Challenge-V2-Julio-Mixco/static/js/2.a57aba5c.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Ravn-Challenge-V2-Julio-Mixco/static/js/2.a57aba5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829af645e95cf157225d",
+    "url": "/Ravn-Challenge-V2-Julio-Mixco/static/js/main.08f0f748.chunk.js"
+  },
+  {
+    "revision": "8e32320cb0f84a0c5d46",
+    "url": "/Ravn-Challenge-V2-Julio-Mixco/static/js/runtime-main.c25f197b.js"
+  }
+]);
